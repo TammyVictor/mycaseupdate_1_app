f6e52391b1c556d19e1345f2732be152131ac8c1
TammyVictor <victor.tam600@gmail.com>
2026-04-30 00:11:55 +0200

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,5 +1,4 @@
 import { chromium } from 'playwright';
-import fetch from 'node-fetch';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
